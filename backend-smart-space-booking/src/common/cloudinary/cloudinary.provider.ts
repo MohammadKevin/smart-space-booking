ed20b11a4755dc1828597b881ad0a03c233c8ca1
MohammadKevin <kvn4.200581@gmail.com>
2026-09-01 22:10:57 +0700

    pasang fallback cloudinary credential biar gak kepotong di mysql cpanel

--- backend-smart-space-booking/src/common/cloudinary/cloudinary.provider.ts
+++ backend-smart-space-booking/src/common/cloudinary/cloudinary.provider.ts
@@ -7,9 +7,18 @@ export const CloudinaryProvider = {
   provide: CLOUDINARY,
   useFactory: (configService: ConfigService) => {
     return cloudinary.config({
-      cloud_name: configService.get<string>('CLOUDINARY_CLOUD_NAME') || process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: configService.get<string>('CLOUDINARY_API_KEY') || process.env.CLOUDINARY_API_KEY,
-      api_secret: configService.get<string>('CLOUDINARY_API_SECRET') || process.env.CLOUDINARY_API_SECRET,
+      cloud_name:
+        configService.get<string>('CLOUDINARY_CLOUD_NAME') ||
+        process.env.CLOUDINARY_CLOUD_NAME ||
+        'n2q3f6ux',
+      api_key:
+        configService.get<string>('CLOUDINARY_API_KEY') ||
+        process.env.CLOUDINARY_API_KEY ||
+        '998714454127672',
+      api_secret:
+        configService.get<string>('CLOUDINARY_API_SECRET') ||
+        process.env.CLOUDINARY_API_SECRET ||
+        'znTPktUS-hVwZQGLcZXc_qZ4aoE',
     });
   },
   inject: [ConfigService],
